ffbe9ed007dc831c641bd0d711166fbfab91bb4e
istarlet <istarlet@gmail.com>
2022-06-19 20:14:31 -0400

    Add JS but audio files don't work

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"]
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 sounds.forEach(sound => {
-    const btn = document.createElement("button")
-    btn.classList.add("btn")
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
 
     btn.innerText = sound
 
-    document.getElementById("buttons").appendChild(btn)
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).onplay()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
 })
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
