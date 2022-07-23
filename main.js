99c356bdcebafa7e00e1631f872d309bf4cbdf61
istarlet <istarlet@gmail.com>
2022-07-22 20:20:41 -0400

    Add new sounds

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
 
-sounds.forEach(sound => {
-    const btn = document.createElement('button')
-    btn.classList.add('btn')
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
 
-    btn.innerText = sound
+  btn.innerText = sound;
 
-    btn.addEventListener('click', () => {
-        stopSongs()
+  btn.addEventListener("click", () => {
+    stopSongs();
 
-        document.getElementById(sound).onplay()
-    })
+    document.getElementById(sound).onplay();
+  });
 
-    document.getElementById('buttons').appendChild(btn)
-})
+  document.getElementById("buttons").appendChild(btn);
+});
 
 function stopSongs() {
-    sounds.forEach(sound => {
-        const song = document.getElementById(sound)
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
 
-        song.pause()
-        song.currentTime = 0;
-    })
+    song.pause();
+    song.currentTime = 0;
+  });
 }
